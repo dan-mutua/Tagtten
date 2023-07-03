abc89ae173bd66bb03ad8d7c1744cdadba869c22
dan-mutua <danmutua15@gmail.com>
2023-07-04 01:54:45 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+(function ($) {
+  "use strict";
+
+  $(function () {
+    $(window).trigger("resize");
+    mainNav();
+  });
+
+  function mainNav() {
+    // Your code for the mainNav function goes here
+  }
+
+  // Only include the code for the index.html section
+  function indexHtmlSection() {
+    // Your code for the index.html section goes here
+  }
+
+  // Call the function for the index.html section
+  indexHtmlSection();
+})(jQuery);
